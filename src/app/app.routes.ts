c4168c38dccfbac2c6ea59ec19da6e7d93ae9f6c
alejandro321ADR <alejmatam19.yui@hotmail.com>
2025-02-09 19:29:07 -0500

    index.html

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,14 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './components/home/home.component';
+import { LoginComponent } from './components/login/login.component';
+import { AboutUsComponent } from './components/about-us/about-us.component';
+import { UserHomeComponent } from './components/user/user-home/user-home.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: "",
+        component: HomeComponent,
+        children:[{path: "login", component: LoginComponent},{path:"", component:AboutUsComponent}]
+    },
+    { path: "userHome/:userId", component: UserHomeComponent }
+];
